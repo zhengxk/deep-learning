45e3b6f11ca8592d521fabced4360abe8368a972
zhengxk <2458262574@qq.com>
2016-12-08 22:37:45 +0800

    add unk class

--- src/corpus/UNK.java
+++ src/corpus/UNK.java
@@ -1,10 +1,16 @@
 package corpus;
 
+import java.io.BufferedReader;
 import java.util.HashSet;
+
+import util.FileUtil;
 
 public class UNK {
 	
 	public static HashSet<String> getDic(){
+		
+		BufferedReader br = FileUtil.getReader("inputpath");
+		
 		
 		return null;
 	}
